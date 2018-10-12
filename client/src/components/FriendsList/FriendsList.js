9ea746b2cf549aa2e3be1a183cd1bf6d5f11dd57
Cbgamefreak <chad.butler64@gmail.com>
2018-10-11 21:48:32 -0400

    stuff

---
+++ client/src/components/FriendsList/FriendsList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./FriendsList.css";
+import { List, ListItem } from "../../components/List";
+
+const FriendsList = props => {
+
+    return(
+        <div className="friend-card">
+        <ListItem>
+       
+            <img className="friend-img" src={props.image} alt={props.name}/>
+            <br></br><br></br>
+            <a href={props.userId} > <button className="otherUser" name={props.name}>{props.name}</button> </a>
+            <hr></hr>
+             <button className="otherUser" onClick={()=> props.handleFriends(props.id)} name={props.name}>unfollow</button>
+            
+            
+
+
+
+          
+        
+        </ListItem>
+        </div>
+    )
+
+}
+
+export default FriendsList;
